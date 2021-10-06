e9f0b718c7f9dd41060401ce62a18ccbd3f75c7f
liuyr88 <52090924+liuyr88@users.noreply.github.com>
2021-10-06 10:25:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,10 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.254/g' package/base-files/files/bin/config_generate
+
+#vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git 
+git clone https://github.com/jerrykuku/luci-app-vssr.git 
+mv luci-app-vssr/ package/
+mv lua-maxminddb/ package/
