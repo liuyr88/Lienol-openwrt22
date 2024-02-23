76bdb7da7c5cf749113a1e7b3aa04de03e5e0b8a
liuyr88 <52090924+liuyr88@users.noreply.github.com>
2024-02-23 12:14:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,7 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.4/g' package/base-files/files/bin/config_generate
+
+cd openwrt
+mkdir package/themes
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/themes/luci-theme-argon
