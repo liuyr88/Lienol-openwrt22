a151db884efe90099a28b68622204e8ef576d0ae
liuyr88 <52090924+liuyr88@users.noreply.github.com>
2025-01-01 10:50:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,9 +12,11 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.254/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Melt-OpenWrt/g' package/base-files/files/bin/config_generate
+sed -i 's/root:::0:99999:7:::/root:$1$m9enpRnu$TjbaRy7zgCDF.hXF6md7p1:20077:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 #cd openwrt
 #mkdir package/themes
 #git clone https://github.com/jerrykuku/luci-theme-argon.git package/themes/luci-theme-argon
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
