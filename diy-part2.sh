ae88a4794fc1c4f67c67a4b5ace481ffaf08385c
liuyr88 <52090924+liuyr88@users.noreply.github.com>
2025-01-08 12:11:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.50.254/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/Melt-OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i 's/root:::0:99999:7:::/root:$1$m9enpRnu$TjbaRy7zgCDF.hXF6md7p1:20077:0:99999:7:::/g' package/base-files/files/etc/shadow
-
+sed -i 's/def_bool y/def_bool n/g' config/Config-build.in
 #cd openwrt
 #mkdir package/themes
 #git clone https://github.com/jerrykuku/luci-theme-argon.git package/themes/luci-theme-argon
